1b0e8168964051fca7311f387a73fd3ad78ebd9b
sjoerdsein <sjoerd@famseinhorst.nl>
2016-03-16 17:25:48 +0100

    Menu links toegevoegd.        AMAZINGgit statusgit status!

---
+++ script.js
@@ -0,0 +1,37 @@
+// $(document).ready(function() {
+//   var numitems =  $("nav li").length;
+//   $("nav ul").css("column-count",Math.round(numitems/2));
+// });
+
+$('[hamburger]').click(function() {
+	$(this).parent().toggleClass('extended');
+});
+
+$('html').click(function() {
+	$('nav').removeClass('extended');
+});
+
+$('nav').click(function(event){
+    event.stopPropagation();
+});
+
+$('nav a').click(function(event){
+	$('nav').removeClass('extended');
+});
+
+$('a').click(function() {
+	console.log($(this).attr('href'));
+});
+
+
+// van http://jsfiddle.net/49u2q/6/
+$.fn.toggleAttr = function(attr, val) {
+	return this.each(function() {
+		var $this = $(this);
+		if ($this.is("[" + attr + "]")) {
+			$this.removeAttr(attr);
+		} else {
+			$this.attr(attr, val);
+		}
+	});
+}
