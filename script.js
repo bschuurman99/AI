4f975befbeb84ac28e33fdc276710af77f0030fb
sjoerdsein <sjoerd@famseinhorst.nl>
2016-03-22 22:01:44 +0100

    Nav bijna perfect en systeem achtergronden aangepast.
    JS geeft ieder element met bg='X' de achtergrond X.jpg. De 'landing
    page' moet nog wel verbeterd worden. Die is alleen wit. Quantum chip
    afbeelding veranderd en afbeelding renderfarm toegevoegd.

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-// $(document).ready(function() {
-//   var numitems =  $("nav li").length;
-//   $("nav ul").css("column-count",Math.round(numitems/2));
-// });
+(function($) {
+	$('[bg]').each(function(){
+		$(this).css('background-image','url(images/'+$(this).attr('bg')+'.jpg)');
+	});
 
-var n = false;
 
-$('[hamburger]').click(function() {
-	$('nav, [icon]').toggleClass('extended');
- 	n = true;
-});
+	$('[hamburger]').click(function() {
+		console.log('hamburger');
+		$('nav, [icon], [scrim]').toggleClass('extended');
+	});
 
-$('nav').click(function() {
-	if (!n) {	$('nav').removeClass('extended');};
-	n = false;
-});
+	$('[scrim]').click(function() {
+		$('nav, [icon], [scrim]').removeClass('extended');
+		console.log('scrim');
+	});
 
-$('nav ul').click(function(event){
-    event.stopPropagation();
-});
+	$('nav ul').click(function(event){
+	    event.stopPropagation();
+	});
 
-$('nav a').click(function(event){
-	$('nav').removeClass('extended');
-});
+	$('nav a').click(function(event){
+		$('nav, [icon], [scrim]').removeClass('extended');
+		$('html, body').stop().animate({scrollTop: Math.round($($(this).attr('href')).offset().top) - ($(window).innerWidth()>1263?67:0)}, 750);
+	});
 
-$('nav a').click(function() {
-  $('html, body').animate({scrollTop: Math.round($($(this).attr('href')).offset().top)}, 750);
-});
+	$('[totop]').click(function(){
+		$('html, body').animate({scrollTop:0},750);
+	});
 
-$('[totop]').click(function(){
-	$('html, body').animate({scrollTop:0},750);
-});
+	var $totop = $('[totop]');
+	var $nav = $('nav');
 
-$(window).scroll(function() {
-		if($(window).scrollTop() > 0) {
-				$('[totop]').removeClass('ontop');
-		} else {
-				$('[totop]').addClass('ontop');
-		}
-});
+	$(window).scroll(function() {
+		var pxFromTop = $(window).scrollTop();
+		var opacity = Math.min(pxFromTop/200, 1);
+
+		if (pxFromTop > 0 && $totop.hasClass('ontop')) $totop.removeClass('ontop');
+			else if (pxFromTop <= 0 && !$totop.hasClass('ontop')) $totop.addClass('ontop');
+
+		$nav.css({'background-color':'rgba(255, 255, 255, '+ opacity +')','box-shadow':'0px 0px 6px rgba(0, 0, 0, ' + opacity * 0.6 + ')'});
+	});
+
+	setTimeout(function(){ if (window.location.hash && $(window).innerWidth()>1263) $('html, body').stop().animate({scrollTop: Math.round($(window.location.hash).offset().top) - 67}, 100);},500);
+}($));
